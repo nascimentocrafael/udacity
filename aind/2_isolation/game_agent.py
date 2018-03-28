f1e76ad725dfdd331f003fe54cc4d3632034a307
Rafael Nascimento <rafael.correian@gmail.com>
2018-03-28 13:27:14 -0300

    first version of alphabeta

--- aind/2_isolation/game_agent.py
+++ aind/2_isolation/game_agent.py
@@ -186,11 +186,7 @@ class MinimaxPlayer(IsolationPlayer):
         otherwise return the minimum value over all legal child
         nodes.
         """
-        #if self.time_left() < self.TIMER_THRESHOLD:
-        #    raise SearchTimeout()
-        if self.terminal_test(game):
-            return self.score(game, self)
-        if depth == 0:
+        if self.terminal_test(game) or depth == 0:
             return self.score(game, self)
         v = float("inf")
         for m in game.get_legal_moves():
@@ -203,11 +199,7 @@ class MinimaxPlayer(IsolationPlayer):
         otherwise return the maximum value over all legal child
         nodes.
         """
-        #if self.time_left() < self.TIMER_THRESHOLD:
-        #    raise SearchTimeout()
-        if self.terminal_test(game):
-            return self.score(game, self)
-        if depth == 0:
+        if self.terminal_test(game) or depth == 0:
             return self.score(game, self)
         v = float("-inf")
         for m in game.get_legal_moves():
@@ -255,7 +247,7 @@ class MinimaxPlayer(IsolationPlayer):
         """
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
-            
+
         return max(game.get_legal_moves(),
                     key=lambda m: self.min_value(game.forecast_move(m), depth-1))
 
@@ -300,7 +292,63 @@ class AlphaBetaPlayer(IsolationPlayer):
         self.time_left = time_left
 
         # TODO: finish this function!
-        raise NotImplementedError
+        # Initialize the best move so that this function returns something
+        # in case the search fails due to timeout
+        best_move = (-1, -1)
+
+        try:
+            # The try/except block will automatically catch the exception
+            # raised when the timer is about to expire.
+            return self.alphabeta(game, self.search_depth)
+
+        except SearchTimeout:
+            pass  # Handle any actions required after timeout as needed
+
+        # Return the best move from the last completed search iteration
+        return best_move
+
+    def terminal_test(self, game):
+        """ Return True if the game is over for the active player
+        and False otherwise.
+        """
+        if self.time_left() < self.TIMER_THRESHOLD:
+            raise SearchTimeout()
+
+        moves_available = bool(game.get_legal_moves())  # by Assumption 1
+        return not moves_available
+    
+    def min_value(self, game, depth, alpha, beta):
+        """ Return the value for a win (+1) if the game is over,
+        otherwise return the minimum value over all legal child
+        nodes.
+        """
+        if self.terminal_test(game) or depth == 0:
+            return self.score(game, self)
+        v = float("inf")
+        for m in game.get_legal_moves():
+            result = self.max_value(game.forecast_move(m), depth-1, alpha, beta)
+            v = min(v, result)
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+    
+    
+    def max_value(self, game, depth, alpha, beta):
+        """ Return the value for a loss (-1) if the game is over,
+        otherwise return the maximum value over all legal child
+        nodes.
+        """
+        if self.terminal_test(game) or depth == 0:
+            return self.score(game, self)
+        v = float("-inf")
+        for m in game.get_legal_moves():
+            result = self.min_value(game.forecast_move(m), depth-1, alpha, beta)
+            v = max(v, result)
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
 
     def alphabeta(self, game, depth, alpha=float("-inf"), beta=float("inf")):
         """Implement depth-limited minimax search with alpha-beta pruning as
@@ -351,4 +399,6 @@ class AlphaBetaPlayer(IsolationPlayer):
             raise SearchTimeout()
 
         # TODO: finish this function!
-        raise NotImplementedError
+        return max(game.get_legal_moves(),
+                    key=lambda m: self.max_value(game.forecast_move(m), depth-1, alpha, beta))
+
