cbd875962edc81b95b0ef67e4dca3f3119c52359
Rafael Nascimento <rafael.correian@gmail.com>
2019-09-16 11:33:44 -0300

    notebooks 0 and 1 ok. first version of model.py with init and forward definitions of DecoderRNN

--- cvnd/P2_Image-Captioning-Project-master/model.py
+++ cvnd/P2_Image-Captioning-Project-master/model.py
@@ -23,11 +23,47 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        ''' Initialize the layers of this model.'''
+        super(DecoderRNN, self).__init__()
+        
+        self.hidden_dim = hidden_size
+
+        # embedding layer that turns words into a vector of a specified size
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
+
+        # the LSTM takes embedded features+word_embeddings (of a specified size) as inputs 
+        # and outputs hidden states of size hidden_dim
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+
+        # the linear layer that maps the hidden state output dimension 
+        # to the number of words in the vocabulary
+        # the last layer gives the probability of the next word
+        self.hidden2vocab = nn.Linear(hidden_size, vocab_size)
+  
+
     def forward(self, features, captions):
-        pass
+        #pass
+        ''' Define the feedforward behavior of the model.'''
+        # create embedded word vectors for each word in a sentence
+        embeds = self.word_embeddings(captions[:, :-1])
+
+        # append caption embeds to the end of feature vector
+        # after embedding, caption have shape (batch_size, caption_len, embedding_dim)
+        # because of that, it is necessary to insert one dimension to the image's features
+        # features will have shape (batch_size, 1, features_len)
+        lstm_input = torch.cat((features.unsqueeze(dim=1), embeds), 1)
+
+        # the lstm takes in our embeddings and hiddent state
+        lstm_out, _ = self.lstm(lstm_input)
+
+        # get the scores for the most likely words
+        word_outputs = self.hidden2vocab(lstm_out)
+        #word_scores = F.log_softmax(word_outputs, dim=1)
+        
+        return word_outputs
+        
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
         pass
+
